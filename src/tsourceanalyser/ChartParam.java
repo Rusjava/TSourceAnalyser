4c4d8819ae91820cdf0fb59a77077b46e5e1b3b2
Samsung <Samsung@127.0.0.1>
2015-03-19 09:21:57 +0400

    Moved some code into ChartParam class

--- src/tsourceanalyser/ChartParam.java
+++ src/tsourceanalyser/ChartParam.java
@@ -11,29 +11,61 @@ package tsourceanalyser;
  * @version 1.0
  */
 public class ChartParam {
+
     public final String key;
-    public final double [] lineData;
-    public final double [] data;
+    public final double[] data;
     public final double step;
     public final double offset;
     public final int size;
-    public double meanValue;
-    public double meanDeviation;
-    
+    private double meanValue;
+    private double meanDeviation;
+    private int nel = 0;
+
     /**
      * Constructor
+     *
      * @param key chart key
      * @param step plot step
      * @param size plot size
      * @param offset plot offset
      */
-    public ChartParam (String key, double step, int size, double offset) {
+    public ChartParam(String key, double step, int size, double offset) {
         super();
-        this.key=key;
-        this.size=size;
-        this.step=step;
-        this.offset=offset;
-        lineData=new double [size];
-        data=new double [size];
+        this.key = key;
+        this.size = size;
+        this.step = step;
+        this.offset = offset;
+        this.data = new double[size];
+    }
+
+    /**
+     * Adding data for one electron
+     *
+     * @param electron
+     */
+    public void add(double electron) {
+        int index = (int) Math.round((electron - offset) / step);
+        if (index >= 0 && index < size) {
+            data[index]++;
+        }
+        nel++;
+        meanValue += electron;
+        meanDeviation += electron * electron;
+    }
+
+    /**
+     * Returns mean value
+     * @return
+     */
+    public double getMeanValue() {
+        return meanValue / nel;
+    }
+    
+    /**
+     * Returns mean deviation
+     * @return
+     */
+    public double getMeanDeviation() {
+        return Math.sqrt((meanDeviation - meanValue * meanValue /nel) / nel);
     }
 }
